7d7b4ad1e63d919295fa83a111c7b23c22fabdb6
Alexandru-Marian Constantin <alexm.constantin@gmail.com>
2025-07-16 21:00:29 +0300

    Revert to commit 52aa9e1 - clean state with RAILPACK builder

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT || '5173'),
-  },
-  server: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT || '5173'),
-  }
 })
